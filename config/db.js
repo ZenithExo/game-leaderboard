0ce00d1a49157641608b2063076c07ef8361d292
ZenithExo <zenithexodus55@gmail.com>
2025-04-09 11:25:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,9 +9,27 @@ const connectDB = async () => {
     });
     console.log('MongoDB connected successfully');
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from DB');
+});
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  console.log('Mongoose connection closed due to app termination');
+  process.exit(0);
+});
 
 module.exports = connectDB;
