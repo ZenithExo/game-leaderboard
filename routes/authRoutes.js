d0f8cd0d3a95558b69f7e7bd8fb1f96b2f9e18f9
ZenithExo <zenithexodus55@gmail.com>
2025-04-18 11:46:03 +0530

    Fixing errors

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
+const authRoutes = require('./routes/authRoutes');
+app.use('/auth', authRoutes);
+
 
 router.post('/register', register);
 router.post('/login', login);
